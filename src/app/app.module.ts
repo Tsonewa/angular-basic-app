05c247269eb046c6f6c9440f33f37f299a871e01
Tsonewa <tsonewa@gmail.com>
2021-12-28 00:34:21 +0200

    basic alerts components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SuccessAlert } from './success.alert/success.alert.component';
+import { WarningAlert } from './warning.alert/warning.alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuccessAlert,
+    WarningAlert
   ],
   imports: [
     BrowserModule
