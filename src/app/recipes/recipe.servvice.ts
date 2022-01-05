c24226d3236e04fe206badda60c52372ea13d2e3
Tsonewa <tsonewa@gmail.com>
2022-01-05 22:20:32 +0200

    add ingredients to recipes

--- src/app/recipes/recipe.servvice.ts
+++ src/app/recipes/recipe.servvice.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../ingredient/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService{
@@ -7,8 +8,10 @@ recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [new Recipe
         ('Spaghetti',
          'This simple spaghetti and meat sauce is bursting with flavor and will satisfy the most', 
-         'https://a7m3f5i5.rocketcdn.me/wp-content/uploads/2015/09/moms-spaghetti-sauce-recipe-a-healthy-slice-of-life-6-of-6-800x600.jpg'), 
-        new Recipe('Pizza Pepperoni', 'Homemade pizza pepperoni description', 'https://www.simplyrecipes.com/thmb/qu-AslBeskzh_HG9H0dQAmcrdLQ=/648x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__09__easy-pepperoni-pizza-lead-4-82c60893fcad4ade906a8a9f59b8da9d.jpg')];
+         'https://a7m3f5i5.rocketcdn.me/wp-content/uploads/2015/09/moms-spaghetti-sauce-recipe-a-healthy-slice-of-life-6-of-6-800x600.jpg', [new Ingredient('Pasta', 2),  new Ingredient('Tomatos', 5), new Ingredient('Cheese', 1)]), 
+        new Recipe('Pizza Pepperoni', 'Homemade pizza pepperoni description', 
+        'https://www.simplyrecipes.com/thmb/qu-AslBeskzh_HG9H0dQAmcrdLQ=/648x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__09__easy-pepperoni-pizza-lead-4-82c60893fcad4ade906a8a9f59b8da9d.jpg', 
+        [new Ingredient('Flour', 2),  new Ingredient('Pepperoni', 5), new Ingredient('Cheese', 2)])];
 
 
         getRecipes(){
