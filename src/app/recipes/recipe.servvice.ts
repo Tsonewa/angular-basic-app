106b6a431887dd4d0b685257ef6e8f8dbcac7c45
Tsonewa <tsonewa@gmail.com>
2022-01-05 21:31:28 +0200

    managing recipes in a recipe

--- src/app/recipes/recipe.servvice.ts
+++ src/app/recipes/recipe.servvice.ts
@@ -1,3 +1,15 @@
+import { Recipe } from './recipe.model';
+
 export class RecipeService{
-    
-}
+  private recipes: Recipe[] = [new Recipe
+        ('Spaghetti',
+         'This simple spaghetti and meat sauce is bursting with flavor and will satisfy the most', 
+         'https://a7m3f5i5.rocketcdn.me/wp-content/uploads/2015/09/moms-spaghetti-sauce-recipe-a-healthy-slice-of-life-6-of-6-800x600.jpg'), 
+        new Recipe('Pizza Pepperoni', 'Homemade pizza pepperoni description', 'https://www.simplyrecipes.com/thmb/qu-AslBeskzh_HG9H0dQAmcrdLQ=/648x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__09__easy-pepperoni-pizza-lead-4-82c60893fcad4ade906a8a9f59b8da9d.jpg')];
+
+
+        getRecipes(){
+            return this.recipes.slice();
+        }
+
+    }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { RecipeService } from './recipe.servvice';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css']
+  styleUrls: ['./recipes.component.css'],
+  providers: [RecipeService]
 })
 export class RecipesComponent implements OnInit {
   selectedRecipe: Recipe;
   @Input() recipe: Recipe;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
